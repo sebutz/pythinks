e3293b44abc2ed7834b9949d5a6612e7f0758573
freemitive@gmx.com <freemitive@gmx.com>
2019-01-17 10:49:52 +0200

    chapo1

---
+++ chapo1.py
@@ -0,0 +1,33 @@
+# chapter1
+
+# first program
+print("Think tank")
+
+# arithmetic ops
+adding = 12 + 13; print(adding)
+mult = 12 * 2; print(mult)
+power = 12 ** 3  # 12 * 12 * 12
+print(power)
+
+substr = 11 - 123;print(substr)
+
+# what if
+print(12 - - 2) # interpreted as +
+print(12 ++ 2) # ignoring one +
+
+
+divide = 123 / 23 ; print(divide)
+divisor = 123 // 23 ; print(divisor)
+remains = 123 % 23 ; print(remains) # 123 = 23 * 5 + 8
+
+
+# tools
+# find out the type (in Python everything is a type)
+print(type(123))
+print(type("Hello world"))
+
+# weird stuff: don't assume
+bigInt = 1, 000, 000 # Python is interpreting (1, 0, 0)
+print(type(bigInt)) # <class 'tuple'>
+print(bigInt)  # (1, 0, 0)
+

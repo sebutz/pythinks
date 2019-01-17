573e05087fde21dfb4db10e3a57130ace580131b
freemitive@gmx.com <freemitive@gmx.com>
2019-01-17 12:20:38 +0200

    chapo2

---
+++ chapo2.py
@@ -0,0 +1,43 @@
+# statement
+# produce an effect
+
+# saving the value of 23 in variable n
+n = 23
+print(n)
+print(type(n))
+print(id(n))
+
+# Python does not care about changing type of a variable (duck type)
+n = 23.4
+print(n)
+print(type(n))
+print(id(n))
+
+'''
+23
+<class 'int'>
+4304456496
+23.4
+<class 'float'>
+4326465944
+
+Process finished with exit code 0
+
+'''
+
+# order of operations : need school for that
+
+# string operations
+s = "aaa" + "bbb"
+print(s)
+
+# illegal s = "aaa" - "baa"
+
+s = "aba" * 2
+print(s)  # abaaba
+
+
+
+
+
+
